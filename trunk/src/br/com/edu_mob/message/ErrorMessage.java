88b5160d0fce02a251f21ee9e882a64c4d3bb6e9
fabioaugustolp@gmail.com <fabioaugustolp@gmail.com@d3a191b3-2ee1-0ca4-921c-0ce78c817d92>
2015-03-20 00:27:11 +0000

    CRUD ALUNO

--- trunk/src/br/com/edu_mob/message/ErrorMessage.java
+++ trunk/src/br/com/edu_mob/message/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
 					"erro_perfil_nome_existente"), PERFIL_FUNCIONALIDADE_OBRIGATORIA("erro_perfil_funcionalidade_obrigatoria"), USUARIO_EMAIL_EXISTENTE(
 							"erro_usuario_email_existente"), USUARIO_CPF_EXISTENTE("erro_usuario_cpf_existente"), USUARIO_CPF_INVALIDO(
 									"erro_usuario_cpf_invalido"), USUARIO_EMAIL_INVALIDO("erro_usuario_email_invalido"), CATEGORIA_NOME_EXISTENTE("erro_categoria_nome_existente"),
-									DEPENDENCIA_EXISTENTE("erro_dependencia_entidade"), DADOS_INVALIDOS("erro_dados_invalidos"),RECUPERAR_SENHA_EMAIL_INVALIDO("erro_email_n_corresponde");
+									DEPENDENCIA_EXISTENTE("erro_dependencia_entidade"), DADOS_INVALIDOS("erro_dados_invalidos"),RECUPERAR_SENHA_EMAIL_INVALIDO("erro_email_n_corresponde"), ALUNO_DATA_NASCIMENTO_MAIOR_DATA_ATUAL("erro_aluno_data_nascimento_invalida");
 
 	private ErrorMessage(String chave) {
 		this.chave = chave;
