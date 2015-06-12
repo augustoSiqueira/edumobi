cdb5718e7a6e5df418d9d5665587f3f37fb775b8
fabioaugustolp@gmail.com <fabioaugustolp@gmail.com@d3a191b3-2ee1-0ca4-921c-0ce78c817d92>
2015-06-12 23:30:44 +0000

    AJUSTE RECUPERACAO DE SENHA

--- trunk/src/br/com/edu_mob/message/SucessMessage.java
+++ trunk/src/br/com/edu_mob/message/SucessMessage.java
@@ -2,9 +2,8 @@ package br.com.edu_mob.message;
 
 public enum SucessMessage {
 
-	SUCESSO("app_titulo_sucesso"), CADASTRADO_SUCESSO("info_cadastro_sucesso"), ATUALIZADO_SUCESSO(
-			"info_atualizado_sucesso"), EXCLUIDO_SUCESSO("info_excluido_sucesso"), CADASTRADA_SUCESSO("info_cadastrada_sucesso"), EXCLUIDA_SUCESSO("info_excluida_sucesso"), ATUALIZADA_SUCESSO("info_atualizada_sucesso")
-			,SENHA_REC("senha_rec");
+	SUCESSO("app_titulo_sucesso"), SENHA_ENVIADA_SUCESSO("info_senha_enviada_sucesso"), CADASTRADO_SUCESSO("info_cadastro_sucesso"), ATUALIZADO_SUCESSO(
+			"info_atualizado_sucesso"), EXCLUIDO_SUCESSO("info_excluido_sucesso"), CADASTRADA_SUCESSO("info_cadastrada_sucesso"), EXCLUIDA_SUCESSO("info_excluida_sucesso"), ATUALIZADA_SUCESSO("info_atualizada_sucesso");
 
 	private SucessMessage(String valor) {
 		this.valor = valor;
